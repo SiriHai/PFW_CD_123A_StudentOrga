fa2a1e76963f8210d0940b87575403b55a6fe5c6
SiriHai <iris.hanheide@fhdw.de>
2023-07-05 16:51:38 +0200

    01_StudentOrga

--- demo/StringDemo.java
+++ demo/StringDemo.java
@@ -9,12 +9,12 @@ public class StringDemo {
     public static void main(String[] args) throws Exception {
         String s1 = "FHDW";
         String s2 = "FHDW";
-        String s_1 = new String("FHDW");
-        String s_2 = new String("FHDW");
+        String s1a = new String("FHDW");
+        String s2a = new String("FHDW");
         System.out.println(s1 == s2); // true
-        System.out.println(s_1 == s_2); // false
-        System.out.println(s1 == s_1); // false
-        System.out.println(s1.equals(s_1)); // true
+        System.out.println(s1a == s2a); // false
+        System.out.println(s1 == s1a); // false
+        System.out.println(s1.equals(s1a)); // true
 
     }
 }
