ff92efdcb64d2c28a78bbb09e0a7a47c82259034
SiriHai <iris.hanheide@fhdw.de>
2023-07-06 12:36:27 +0200

    Konstruktoren

--- src/tests/StudentTest.java
+++ src/tests/StudentTest.java
@@ -4,9 +4,10 @@ import model.Student;
 
 public class StudentTest {
     public static void main(String[] args) throws Exception {
-        Student student1 = new Student();
-        student1.setName("Nathan Evans");
-        student1.setId(1001);
+        Student student1 = new Student("Nathan Evans", 1001);
+        //student1.setName("Nathan Evans");
+        //student1.setName("Nathan", "Evans");
+        //student1.setId(1001);
 
         Student student2 = new Student();
         student2.setName("Miksu Macloud");
@@ -18,12 +19,12 @@ public class StudentTest {
         // Einlesen von der Konsole
         Student student3 = new Student();
         System.out.print("Name: ");
-        String name = System.console().readLine();
-        student3.setName(name);
+        //String name = System.console().readLine();
+        //student3.setName(name);
 
         System.out.print("Id: ");
-        int id3 = Integer.parseInt(System.console().readLine());
-        student3.setId(id3);
+        //int id3 = Integer.parseInt(System.console().readLine());
+        //student3.setId(id3);
 
         System.out.println(student3.info());
     }
